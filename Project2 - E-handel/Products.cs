0fc193f8b35ddb879f3aa1b325471070566a7a67
annaschelin87 <annaschelin87@gmail.com>
2018-07-03 14:06:26 +0200

    Klasser

---
+++ Project2 - E-handel/Products.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project2___E_handel
+{
+    public class Products
+    {
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public string ArticleNr { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+
+        public Products(string productName, double price, string articleNr, string description, string category)
+        {
+            ProductName = productName;
+            Price = price;
+            ArticleNr = articleNr;
+            Description = description;
+            Category = category;
+        }
+    }
+}
--- Project2 - E-handel/User.cs
+++ Project2 - E-handel/User.cs
@@ -7,7 +7,27 @@ namespace Project2___E_handel
 {
     public class User
     {
-        public int security { get; private set; }
-        public string MyProperty { get; set; }
+        public int Security { get; private set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string SSN { get; private set; }
+        public string Street { get; set; }
+        public string Zipcode { get; set; }
+        public string City { get; set; }
+
+        public User(int security, string firstName, string lastName, string email, string password, string ssn, string street, string zipcode, string city)
+        {
+            Security = security;
+            Firstname = firstName;
+            Lastname = lastName;
+            Email = email;
+            Password = password;
+            SSN = ssn;
+            Street = street;
+            Zipcode = zipcode;
+            City = city;
+        }
     }
 }
