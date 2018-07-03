05c9e1c0d1ed34ec6d0b43d3cb8a5f75ea82fc22
annaschelin87 <annaschelin87@gmail.com>
2018-07-03 13:08:56 +0200

    first commit

---
+++ Project2 - E-handel/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project2___E_handel
+{
+    public class User
+    {
+        public int security { get; private set; }
+        public string MyProperty { get; set; }
+    }
+}
