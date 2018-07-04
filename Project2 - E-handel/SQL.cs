c656d261b954f353d0785f7c5e7664262d6dca3c
annaschelin87 <annaschelin87@gmail.com>
2018-07-04 12:38:03 +0200

    SQL methods - still bug in createcontact

--- Project2 - E-handel/SQL.cs
+++ Project2 - E-handel/SQL.cs
@@ -46,6 +46,128 @@ namespace Project2___E_handel
             return products;
 
         }
+        public static int AddProduct(string productName, double price, string description, string category)
+        {
+            int articleNr = 0;
+
+            SqlConnection myConnection = new SqlConnection(connectionString);
+            SqlCommand myCommand = new SqlCommand("AddProduct", myConnection);
+            myCommand.CommandType = System.Data.CommandType.StoredProcedure;
+
+            SqlParameter ProductNameP = new SqlParameter("@Productname", System.Data.SqlDbType.VarChar);
+            ProductNameP.Value = productName;
+            myCommand.Parameters.Add(ProductNameP);
+
+            SqlParameter PriceP = new SqlParameter("@Price", System.Data.SqlDbType.Float);
+            PriceP.Value = price;
+            myCommand.Parameters.Add(PriceP);
+
+            SqlParameter DescriptionP = new SqlParameter("@Description", System.Data.SqlDbType.VarChar);
+            DescriptionP.Value = description;
+            myCommand.Parameters.Add(DescriptionP);
+
+            SqlParameter CategoryP = new SqlParameter("@Category", System.Data.SqlDbType.VarChar);
+            CategoryP.Value = category;
+            myCommand.Parameters.Add(CategoryP);
+
+            SqlParameter ArticleNrP = new SqlParameter("@ArtNbr", System.Data.SqlDbType.Int);
+            ArticleNrP.Direction = System.Data.ParameterDirection.Output;
+            myCommand.Parameters.Add(ArticleNrP);
+
+            try
+            {
+                myConnection.Open();
+                myCommand.ExecuteNonQuery();
+                articleNr = Convert.ToInt32(ArticleNrP.Value);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                myConnection.Close();
+            }
+            return articleNr;
+        }
+        public static int UpdateProduct(int articleNr, string productName, double price, string description, string category)
+        {
+            int succes = 2;
+
+            SqlConnection myConnection = new SqlConnection(connectionString);
+            SqlCommand myCommand = new SqlCommand("UpdateProduct", myConnection);
+            myCommand.CommandType = System.Data.CommandType.StoredProcedure;
+
+            SqlParameter ProductNameP = new SqlParameter("@Productname", System.Data.SqlDbType.VarChar);
+            ProductNameP.Value = productName;
+            myCommand.Parameters.Add(ProductNameP);
+
+            SqlParameter PriceP = new SqlParameter("@Price", System.Data.SqlDbType.Float);
+            PriceP.Value = price;
+            myCommand.Parameters.Add(PriceP);
+
+            SqlParameter DescriptionP = new SqlParameter("@Description", System.Data.SqlDbType.VarChar);
+            DescriptionP.Value = description;
+            myCommand.Parameters.Add(DescriptionP);
+            
+            SqlParameter CategoryP = new SqlParameter("@Category", System.Data.SqlDbType.VarChar);
+            CategoryP.Value = category;
+            myCommand.Parameters.Add(CategoryP);
+
+            SqlParameter ArticleNrP = new SqlParameter("@ArtNbr", System.Data.SqlDbType.Int);
+            ArticleNrP.Value = articleNr;
+            myCommand.Parameters.Add(ArticleNrP);
+
+            SqlParameter SuccesP = new SqlParameter("@SUCCEED", System.Data.SqlDbType.Int);
+            SuccesP.Direction = System.Data.ParameterDirection.Output;
+            myCommand.Parameters.Add(SuccesP);
+            try
+            {
+                myConnection.Open();
+                myCommand.ExecuteNonQuery();
+                succes = Convert.ToInt32(SuccesP.Value);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                myConnection.Close();
+            }
+            return succes;
+        }
+        public static int DeleteProduct(int articleNr)
+        {
+            int succes = 2;
+            SqlConnection myConnection = new SqlConnection(connectionString);
+            SqlCommand myCommand = new SqlCommand("DeleteProduct", myConnection);
+            myCommand.CommandType = System.Data.CommandType.StoredProcedure;
+
+            SqlParameter ArticleNrP = new SqlParameter("@ArtNbr", System.Data.SqlDbType.Int);
+            ArticleNrP.Value = articleNr;
+            myCommand.Parameters.Add(ArticleNrP);
+
+            SqlParameter SuccesP = new SqlParameter("@SUCCEED", System.Data.SqlDbType.Int);
+            SuccesP.Direction = System.Data.ParameterDirection.Output;
+            myCommand.Parameters.Add(SuccesP);
+
+            try
+            {
+                myConnection.Open();
+                myCommand.ExecuteNonQuery();
+                succes = Convert.ToInt32(SuccesP.Value);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                myConnection.Close();
+            }
+            return succes;
+        }
         public static void CreateUser(int security, string firstName, string lastName, string email, string password, string ssn, string street, string zipcode, string city)
         {
             SqlConnection myConnection = new SqlConnection(connectionString);
@@ -87,12 +209,12 @@ namespace Project2___E_handel
             SqlParameter cityP = new SqlParameter("@City", System.Data.SqlDbType.VarChar);
             cityP.Value = city;
             myCommand.Parameters.Add(cityP);
-            
+
             try
             {
                 myConnection.Open();
                 myCommand.ExecuteNonQuery();
-                
+
             }
             catch (Exception ex)
             {
@@ -102,47 +224,7 @@ namespace Project2___E_handel
             {
                 myConnection.Close();
             }
-            
-        }
-        public static int AddProduct(string productName, double price, string description, string category)
-        {
-            int articleNr = 0;
-         
-            SqlConnection myConnection = new SqlConnection(connectionString);
-            SqlCommand myCommand = new SqlCommand("AddProduct", myConnection);
-            myCommand.CommandType = System.Data.CommandType.StoredProcedure;
 
-            SqlParameter ProductNameP = new SqlParameter("@Productname", System.Data.SqlDbType.VarChar);
-            ProductNameP.Value = productName;
-            myCommand.Parameters.Add(ProductNameP);
-            
-            SqlParameter PriceP = new SqlParameter("@Price", System.Data.SqlDbType.Float);
-            PriceP.Value = price;
-            myCommand.Parameters.Add(PriceP);
-
-            SqlParameter DescriptionP = new SqlParameter("@Description", System.Data.SqlDbType.VarChar);
-            DescriptionP.Value = description;
-            myCommand.Parameters.Add(DescriptionP);
-
-            SqlParameter ArticleNrP = new SqlParameter("@ArtNbr", System.Data.SqlDbType.Int);
-            ArticleNrP.Direction = System.Data.ParameterDirection.Output;
-            myCommand.Parameters.Add(ArticleNrP);
-
-            try
-            {
-                myConnection.Open();
-                myCommand.ExecuteNonQuery();
-                articleNr = Convert.ToInt32(ArticleNrP.Value);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-            finally
-            {
-                myConnection.Close();
-            }
-            return articleNr;
         }
 
     }
