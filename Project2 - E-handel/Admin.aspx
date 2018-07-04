bfdfcfe35b7ef9e1cd7cf6a65636ef47644e1223
annaschelin87 <annaschelin87@gmail.com>
2018-07-04 14:59:41 +0200

    admin

---
+++ Project2 - E-handel/Admin.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Admin.aspx.cs" Inherits="Project2___E_handel.Admin" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:ListBox ID="ListBoxProducts" runat="server" Height="400px" Width="250px" OnSelectedIndexChanged="ListBoxProducts_SelectedIndexChanged"></asp:ListBox>
+    <asp:TextBox ID="TextBoxProductName" runat="server"></asp:TextBox>
+</asp:Content>
---
+++ Project2 - E-handel/Admin.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Project2___E_handel
+{
+    public partial class Admin : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                foreach (var p in SQL.GetAllProducts())
+                {
+                    ListBoxProducts.Items.Add($"Artnr. {p.ArticleNr} {p.ProductName}, {p.Price}:-");
+                }
+
+            }
+        }
+
+        protected void ListBoxProducts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ListBoxProducts.SelectedIndex >= 0)
+            {
+                //var p = SQL.GetAllProducts()[ListBoxProducts.SelectedIndex];
+
+                //TextBoxFirstname.Text = p.Firstname;
+                //TextBoxLastname.Text = p.Lastname;
+                //TextBoxSSN.Text = p.SSN;
+
+
+            }
+        }
+    }
+}
